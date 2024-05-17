17893652559167e18bf61d281bc7d4fa69673f90
Hwan Jun Kim <spdlqjdkdlel1702@naver.com>
2024-05-17 14:39:52 +0900

    [feat] GlobalExceptinoHandler 메소드 추가 및 JpaAuditing 세팅

--- mobile3/src/main/java/sopkathon/mobile3/common/GlobalExceptionHandler.java
+++ mobile3/src/main/java/sopkathon/mobile3/common/GlobalExceptionHandler.java
@@ -1,8 +1,25 @@
 package sopkathon.mobile3.common;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import sopkathon.mobile3.common.dto.ErrorResponse;
+
+import java.util.Objects;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(EntityNotFoundException.class)
+    protected ResponseEntity<ErrorResponse> handleEntityNotFoundException(EntityNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponse.of(HttpStatus.NOT_FOUND.value(), e.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse.of(HttpStatus.BAD_REQUEST.value(), Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage()));
+    }
 }
