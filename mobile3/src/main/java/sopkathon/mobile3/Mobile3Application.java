f8ffc8178515493c92fb48579de07851c7edd5bf
Hwan Jun Kim <spdlqjdkdlel1702@naver.com>
2024-05-17 14:05:11 +0900

    프로젝트 구조 세팅

---
+++ mobile3/src/main/java/sopkathon/mobile3/Mobile3Application.java
@@ -0,0 +1,13 @@
+package sopkathon.mobile3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mobile3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mobile3Application.class, args);
+	}
+
+}
