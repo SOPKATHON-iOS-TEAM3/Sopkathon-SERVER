8797860517d477e0dfa73aea5abc029de3287ff4
Hwan Jun Kim <spdlqjdkdlel1702@naver.com>
2024-05-18 19:18:53 +0900

    ymlcnrk

---
+++ mobile3/src/test/java/sopkathon/mobile3/Mobile3ApplicationTests.java
@@ -0,0 +1,13 @@
+package sopkathon.mobile3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mobile3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
