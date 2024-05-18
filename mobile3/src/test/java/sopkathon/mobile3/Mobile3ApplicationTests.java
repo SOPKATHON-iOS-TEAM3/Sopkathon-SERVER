97c5822dbb4b1b1fe65496eda0f14e6759582c31
DongHoon Lee <dhl1924@naver.com>
2024-05-19 00:58:27 +0900

    HOTFIX: 테스트 빌드를 위한 설정 추가

--- mobile3/src/test/java/sopkathon/mobile3/Mobile3ApplicationTests.java
+++ mobile3/src/test/java/sopkathon/mobile3/Mobile3ApplicationTests.java
@@ -3,7 +3,7 @@ package sopkathon.mobile3;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = Mobile3ApplicationTests.class)
 class Mobile3ApplicationTests {
 
 	@Test
