6b9c14e6210fcabe18a4fc5cee75cb7a3df4b297
DongHoon Lee <dhl1924@naver.com>
2024-05-19 01:36:19 +0900

    HOTFIX: swagger description 수정

--- mobile3/src/main/java/sopkathon/mobile3/swagger/SwaggerConfig.java
+++ mobile3/src/main/java/sopkathon/mobile3/swagger/SwaggerConfig.java
@@ -12,8 +12,8 @@ public class SwaggerConfig {
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("HyundaiCard Project API")
-                        .description("현대카드 서비스를 제공합니다")
+                        .title("To.0(가제) Project API")
+                        .description("서로에 대한 사전 정보를 퀴즈라는 게임식 형태로, 미리 제공함으로써 첫 대화를 수월하게 도와주는 서비스를 제공합니다")
                         .version("1.0.0"));
     }
 }
