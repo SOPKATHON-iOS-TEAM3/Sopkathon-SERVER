47d0a246f995f04b73132c0be82d8e5871064574
DongHoon Lee <dhl1924@naver.com>
2024-05-19 03:12:21 +0900

    feat: 퀴즈를 생성하는 서비스레이어 구현

---
+++ mobile3/src/main/java/sopkathon/mobile3/quiz/service/QuizService.java
@@ -0,0 +1,24 @@
+package sopkathon.mobile3.quiz.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sopkathon.mobile3.member.domain.Member;
+import sopkathon.mobile3.member.service.MemberService;
+import sopkathon.mobile3.quiz.domain.Quiz;
+import sopkathon.mobile3.quiz.repository.QuizRepository;
+
+@Service
+@RequiredArgsConstructor
+public class QuizService {
+
+    private final QuizRepository quizRepository;
+    private final MemberService memberService;
+
+    @Transactional
+    public String createQuiz(Long memberId) {
+        Member member = memberService.findById(memberId);
+        Quiz quiz = quizRepository.save(Quiz.create(null, member));
+        return quiz.getQuizId().toString();
+    }
+}
