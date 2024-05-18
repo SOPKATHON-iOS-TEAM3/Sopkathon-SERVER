87050ae1f580d8240ba2b587477f7a25158ff255
Hwan Jun Kim <spdlqjdkdlel1702@naver.com>
2024-05-19 06:05:44 +0900

    [fix] 홈 반환 시 nickName 추가

--- mobile3/src/main/java/sopkathon/mobile3/member/service/dto/GetMainResponseDto.java
+++ mobile3/src/main/java/sopkathon/mobile3/member/service/dto/GetMainResponseDto.java
@@ -2,9 +2,10 @@ package sopkathon.mobile3.member.service.dto;
 
 import sopkathon.mobile3.member.domain.Member;
 
-public record GetMainResponseDto(Long memberId, int targetFriend) {
+public record GetMainResponseDto(Long memberId, int targetFriend,String nickName) {
 
     public static GetMainResponseDto of(Member member) {
-        return new GetMainResponseDto(member.getMemberId(), member.getTargetFriend() - member.getFriendShipFriend());
+        return new GetMainResponseDto(member.getMemberId(), member.getTargetFriend() - member.getFriendShipFriend()
+        ,member.getNickName());
     }
 }
