03332fb1511a7bbe118d5ebecc1aaaa24135e3a5
DongHoon Lee <dhl1924@naver.com>
2024-05-19 03:16:32 +0900

    chore: Swagger 설명 변경

--- mobile3/src/main/java/sopkathon/mobile3/member/controller/MemberController.java
+++ mobile3/src/main/java/sopkathon/mobile3/member/controller/MemberController.java
@@ -18,7 +18,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/member")
-    @Operation(summary = "닉네임과 타겟 필드로 멤버를 생성하는 API", description = "닉네임과 타겟 필드로 멤버를 생성하는 로직을 처리합니다.")
+    @Operation(summary = "닉네임과 친해지고픈 친구 필드를 이용해 멤버를 생성하는 API", description = "닉네임과 친해지고픈 친구 필드를 이용해 멤버를 생성합니다")
     public ResponseEntity<SuccessStatusResponse> createMember(@RequestBody MemberCreateRequestDto requestDto) {
         return ResponseEntity.ok(memberService.create(requestDto));
     }
